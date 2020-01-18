c10c315df6efffacc3e7643be3afdec627113ef6
tuka <tsukasa@birdcage.jp>
2020-01-18 10:55:23 +0900

    refactor test code

--- lib/check_meta_test.go
+++ lib/check_meta_test.go
@@ -15,16 +15,6 @@ func TestCheckMetaValue(t *testing.T) {
 		status   checkers.Status
 	}{
 		{
-			expected: "foobar",
-			actual:   "foobar",
-			status:   checkers.OK,
-		},
-		{
-			expected: "hoge",
-			actual:   "fuga",
-			status:   checkers.CRITICAL,
-		},
-		{
 			expected: "1000",
 			actual:   float64(1000),
 			status:   checkers.OK,
@@ -60,6 +50,55 @@ func TestCheckMetaValue(t *testing.T) {
 
 	for i, tc := range testCase {
 		opts.Expected = tc.expected
+		chk := checkMetaValue(tc.actual)
+		assert.Equal(t, chk.Status, tc.status, "#%d: Status should be %s", i, tc.status)
+	}
+}
+
+func TestCheckStringTypeValue(t *testing.T) {
+	testCase := []struct {
+		expected string
+		actual   interface{}
+		status   checkers.Status
+		regex    bool
+	}{
+		{
+			expected: "foobar",
+			actual:   "foobar",
+			status:   checkers.OK,
+			regex:    false,
+		},
+		{
+			expected: "hoge",
+			actual:   "fuga",
+			status:   checkers.CRITICAL,
+			regex:    false,
+		},
+		{
+			expected: "foo.*",
+			actual:   "foobar",
+			status:   checkers.CRITICAL,
+			regex:    false,
+		},
+		{
+			expected: "foo.*",
+			actual:   "foobar",
+			status:   checkers.OK,
+			regex:    true,
+		},
+		{
+			expected: "foo[a-z]{3}",
+			actual:   "foobar",
+			status:   checkers.OK,
+			regex:    true,
+		},
+	}
+
+	opts.MetaKey = "dummy"
+
+	for i, tc := range testCase {
+		opts.Expected = tc.expected
+		opts.IsRegex = tc.regex
 		chk := checkMetaValue(tc.actual)
 		assert.Equal(t, chk.Status, tc.status, "#%d: Status should be %s", i, tc.status)
 	}
